55430efa1f5b50f75d22e665596efa9c1a794f77
Tryse Rezza Biantong <tryse.rezza@amarbank.co.id>
2023-10-03 01:02:40 +0700

    Update step_definition SLS-430

--- codecept.conf.js
+++ codecept.conf.js
@@ -37,8 +37,7 @@ exports.config = {
     REST: {
       endpoint: "https://dev-api-sms.otoku.io",
       defaultHeaders: {
-        Authorization:
-          "basic NWY2NjdjMTJmYmJmNjlmNzAwZjdkYzgzNTg0ZTc5ZDI2MmEwODVjMmJmOTIxYzU2MzZjNzgzNTExYzIzNDFhYg==",
+        Authorization: "basic NWY2NjdjMTJmYmJmNjlmNzAwZjdkYzgzNTg0ZTc5ZDI2MmEwODVjMmJmOTIxYzU2MzZjNzgzNTExYzIzNDFhYg==",
         "Content-Type": "application/json",
         Accept: "application/json"
       }
@@ -70,30 +69,6 @@ exports.config = {
     uploadDocLoanPage: "./pages/uploadDocLoan.js",
 
     loanDashboardPage: "./pages/loanDashboard.js",
-
-    onboardingAccOpeningPage: "./pages/onboardingAccOpening.js",
-
-    uploadKtpPage: "./pages/uploadKtp.js",
-
-    formKtpPage: "./pages/formKtp.js",
-
-    resetStateDao: "./dao/resetStateDao.js",
-
-    globalVariable: "./global_variable.js",
-
-    uploadSelfiePage: "./pages/uploadSelfie.js",
-
-    formPersonalDataPage: "./pages/formPersonalData.js",
-
-    formDomicileAddressPage: "./pages/formDomicileAddress.js",
-
-    formEmploymentDataPage: "./pages/formEmploymentData.js",
-
-    formBusinessProfilePage: "./pages/formBusinessProfile.js",
-
-    formBusinessOwnerPage: "./pages/formBusinessOwner.js",
-
-    formBusinessAddressPage: "./pages/formBusinessAddress.js",
   },
   mocha: {},
   bootstrap: null,
@@ -101,38 +76,8 @@ exports.config = {
   teardown: null,
   hooks: [],
   gherkin: {
-    features: ["./features/*.feature",
-      "./features/loanApplication/loanTypeAP/*.feature",
-      "./features/loanApplication/loanTypeAR/*.feature",
-      "./features/loanApplication/loanTypePO/*.feature",
-      "./features/loanDashboard/*.feature",
-      "./features/funding_account_creation/*.feature",
-      "./features/funding_account_opening/01_account_opening_kyc/*.feature",
-      "./features/funding_account_opening/02_account_opening_kyb/*.feature",
-      // "./features/funding_transfer/*.feature",
-      // "./features/funding_create_pin/*.feature",
-    ],
-    steps: ["./step_definitions/steps_registration.js",
-      "./step_definitions/steps_login.js",
-      "./step_definitions/loanApplication/steps_loanType.js",
-      "./step_definitions/loanApplication/steps_selectAnchor.js",
-      "./step_definitions/loanApplication/steps_loanAmountTenor.js",
-      "./step_definitions/loanApplication/steps_loanMonitoring.js",
-      "./step_definitions/steps_login.js",
-      "./step_definitions/steps_forgotPassword.js",
-      "./step_definitions/loanDashboard/loanDashboard.js",
-      "./step_definitions/loanDashboard/historyPage.js",
-      "./step_definitions/steps_onBoardingAccountOpening.js",
-      "./step_definitions/steps_kycUploadEKtp.js",
-      "./step_definitions/steps_kycSubmitFormKtp.js",
-      "./step_definitions/steps_kycUploadSelfie.js",
-      // "./step_definitions/steps_kycSubmitFormDataPersonal.js",
-      // "./step_definitions/steps_kycSubmitFormDomicileAddress.js",
-      // "./step_definitions/steps_kycSubmitFormDataEmployment.js",
-      "./step_definitions/steps_kybSubmitFormBusinessProfile.js",
-      "./step_definitions/steps_kybSubmitFormBusinessOwner.js",
-      "./step_definitions/steps_kybSubmitFormBusinessAddress.js",
-    ],
+    features: ["./features/*.feature", "./features/loanDashboard/*.feature", "./features/loanApplication/loanTypeAP/*.feature", "/features/loanApplication/loanTypeAR/*.feature"],
+    steps: ["./step_definitions/steps.js", "./step_definitions/steps_login.js", "./step_definitions/loanDashboard/loanDashboard.js", "./step_definitions/loanDashboard/historyPage.js"],
   },
   plugins: {
     screenshotOnFail: {
--- steps.d.ts
+++ steps.d.ts
@@ -9,33 +9,19 @@ type headerPage = typeof import('./pages/header.js');
 type whitelistDao = typeof import('./dao/whitelistDao.js');
 type verificationEmailPage = typeof import('./pages/verificationEmail.js');
 type otpDao = typeof import('./dao/otpDao.js');
-type loanTypePage = typeof import('./pages/loanApplication/loanType.js');
-type selectAnchorPage = typeof import('./pages/loanApplication/selectAnchor.js');
-type selectLoanAmountPage = typeof import('./pages/loanApplication/selectLoanAmountTenor.js');
-type loanMonitoringProcessPage = typeof import('./pages/loanApplication/loanMonitoringProcess.js');
 type forgotPasswordPage = typeof import('./pages/forgotPassword.js');
-type uploadDocLoanPage = typeof import('./pages/uploadDocLoan.js');
-type loanDashboardPage = typeof import('./pages/loanDashboard.js');
-type onboardingAccOpeningPage = typeof import('./pages/onboardingAccOpening.js');
-type uploadKtpPage = typeof import('./pages/uploadKtp.js');
-type formKtpPage = typeof import('./pages/formKtp.js');
-type resetStateDao = typeof import('./dao/resetStateDao.js');
-type globalVariable = typeof import('./global_variable.js');
-type uploadSelfiePage = typeof import('./pages/uploadSelfie.js');
-type formPersonalDataPage = typeof import('./pages/formPersonalData.js');
-type formDomicileAddressPage = typeof import('./pages/formDomicileAddress.js');
-type formEmploymentDataPage = typeof import('./pages/formEmploymentData.js');
-type formBusinessProfilePage = typeof import('./pages/formBusinessProfile.js');
-type formBusinessOwnerPage = typeof import('./pages/formBusinessOwner.js');
-type formBusinessAddressPage = typeof import('./pages/formBusinessAddress.js');
 type Appium = import('./helpers/JetpackComposeHelper.js');
 type ChaiWrapper = import('codeceptjs-chai');
+type loanTypePage = typeof import('./pages/loanType.js');
+type selectAnchorPage = typeof import('./pages/selectAnchor.js');
+type selectLoanAmountPage = typeof import('./pages/selectLoanAmountTenor.js');
+type loanMonitoringProcessPage = typeof import('./pages/loanMonitoringProcess.js');
 type loanDashboardPage = typeof import('./pages/loanDashboard.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any, registrationPage: registrationPage, otpConfirmationPage: otpConfirmationPage, loginPage: loginPage, welcomePage: welcomePage, changePhoneNumberPage: changePhoneNumberPage, headerPage: headerPage, whitelistDao: whitelistDao, verificationEmailPage: verificationEmailPage, otpDao: otpDao, loanTypePage: loanTypePage, selectAnchorPage: selectAnchorPage, selectLoanAmountPage: selectLoanAmountPage, loanMonitoringProcessPage: loanMonitoringProcessPage, forgotPasswordPage: forgotPasswordPage, uploadDocLoanPage: uploadDocLoanPage, loanDashboardPage: loanDashboardPage, onboardingAccOpeningPage: onboardingAccOpeningPage, uploadKtpPage: uploadKtpPage, formKtpPage: formKtpPage, resetStateDao: resetStateDao, globalVariable: globalVariable, uploadSelfiePage: uploadSelfiePage, formPersonalDataPage: formPersonalDataPage, formDomicileAddressPage: formDomicileAddressPage, formEmploymentDataPage: formEmploymentDataPage, formBusinessProfilePage: formBusinessProfilePage, formBusinessOwnerPage: formBusinessOwnerPage, formBusinessAddressPage: formBusinessAddressPage }
+  interface SupportObject { I: I, current: any, registrationPage: registrationPage, otpConfirmationPage: otpConfirmationPage, loginPage: loginPage, welcomePage: welcomePage, changePhoneNumberPage: changePhoneNumberPage, headerPage: headerPage, whitelistDao: whitelistDao, verificationEmailPage: verificationEmailPage, otpDao: otpDao, forgotPasswordPage: forgotPasswordPage, loanTypePage: loanTypePage, selectAnchorPage: selectAnchorPage, selectLoanAmountPage: selectLoanAmountPage, loanMonitoringProcessPage: loanMonitoringProcessPage, loanDashboardPage: loanDashboardPage}
   interface Methods extends Appium, ChaiWrapper, REST, JSONResponse {}
-  interface I extends ReturnType<steps_file>, WithTranslation<ChaiWrapper>, WithTranslation<JSONResponse> {}
+  interface I extends ReturnType<steps_file>, WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
   }
