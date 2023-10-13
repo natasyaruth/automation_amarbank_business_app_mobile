65db31227e7ee8380491b4ec420e0a7547fdd895
nurul septariani <nurulseptriani@amarbank.co.id>
2023-10-13 14:07:36 +0700

    scenario for step_definition transfer

---
+++ step_definitions/funding_transfer/steps_transfer.js
@@ -0,0 +1,124 @@
+const {
+    I,
+    transferPage,
+    friendListPage,
+    createPINPage,
+} = inject();
+
+Given("I am a customer who wants to Transfer and has no friend list", () => { });
+
+Given("I am a customer who wants to Transfer and has friend list", () => { });
+
+
+When("I choose category {string}", (category) => {
+    I.wait(2)
+    transferPage.chooseCategory(category);
+});
+
+When("I close bottom sheet category", (closebuttonsheet) => {
+    I.wait(2);
+    transferPage.closesheetcategory(closebuttonsheet);
+});
+
+Then("I see bottom sheet category is dissapear", () => {
+    I.dontSee(transferPage.dropdownLists.category);
+});
+
+When("I input amount higher than active balance", (amount) => {
+    transferPage.inputAmountTransfer(amount);
+});
+
+Then("I can see message {string}", async (messageErrorAmount) => {
+    let actualMessageError = await transferPage.getMessageErroAmount();
+    I.assertEqual(actualMessageError, messageErrorAmount);
+});
+
+When("I input amount {string}", (amount) => {
+    transferPage.inputAmountTransfer(amount);
+});
+
+When("I input notes with {string}", (notes) => {
+    transferPage.fillNotes(notes);
+});
+
+When("I click choose bank transfer service", () => {
+    transferPage.chooseMethodTransfer();
+});
+
+Then("I can see BI Fast and RTOL", () => { });
+I.see(transferPage.radioButtons.methodBifast);
+I.see(transferPage.radioButtons.methodRtol);
+
+Then("I choose transfer service {string}", (rtolService) => {
+    transferPage.chooseRtol(rtolService);
+});
+
+Then("I choose transfer service {string}", (bifastservice) => {
+    transferPage.chooseBifast(bifastservice);
+})
+
+When("I click transfer", () => {
+    transferPage.processTransfer();
+});
+
+Then("I will directly go to page confirmation transfer", () => {
+    I.see(transferPage.texts.amount);
+    I.see(transferPage.texts.service);
+    I.see(transferPage.texts.total);
+    I.see(transferPage.texts.category);
+    I.see(transferPage.texts.note);
+});
+
+When("I input wrong PIN", () => {
+    createPINPage.inputPIN(dummyPIN);
+});
+
+Then("I will be able to see message error {string}", async (expectederrorPINmessage) => {
+    let actualerrorPINmessage = await transferPage.getMessageErrorPIN();
+    I.assertEqual(actualerrorPINmessage, expectederrorPINmessage);
+    createPINPage.tryAgain();
+});
+
+When("I click transfer now", () => {
+    transferPage.confirmTransfer();
+});
+
+Then("I successfully transferred without notes", () => {
+    I.see(transferPage.buttons.copy);
+    I.see(transferPage.buttons.share);
+    I.dontSee(transferPage.texts.note);
+});
+
+Then("i see message {string}", async (errornotes) => {
+    let actualerrornotes = await transferPage.getNotes();
+    I.assertEqual(actualerrornotes, errornotes);
+});
+
+When("I input PIN", (Pin) => {
+    createPINPage.inputPIN(PIN);
+});
+
+Then("Then I successfully transferred", () => {
+    I.see(transferPage.buttons.copy);
+    I.see(transferPage.buttons.share);
+    I.see(transferPage.texts.note);
+    I.see(transferPage.buttons.share);
+});
+
+Then("I see error message {string}", async (erroramount) => {
+    let actualerroramount = await transferPage.getAmountConfirmation();
+    I.assertEqual(actualerroramount,erroramount);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
