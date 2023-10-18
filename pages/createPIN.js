93204a5ecadf6ada792bb85a1b3ba78197dc35b1
Nurul septriani <nurul.septriani@amarbank>
2023-10-18 11:35:14 +0700

    adding new object page for create PIN carry over

--- pages/createPIN.js
+++ pages/createPIN.js
@@ -1,5 +1,3 @@
-const { button } = require("./otpConfirmation");
-
 const { I } = inject();
 
 module.exports = {
@@ -7,17 +5,26 @@ module.exports = {
     password: "~textFieldPassword",
     newPIN: "~textFieldPin",
     otpcode: "~textFieldOtp",
+    confirmPIN: ~"textFieldPin",
   },
   buttons: {
     submitpassword: "~buttonNext",  
     nextpagetransfer: "~buttonNext",
+    tryagainpassword: ~"buttonTry",
+    closesheetpassword: ~"buttonClose",
+    reactiveAccount: ~"buttonReactive",
   },
   icon: {
     eyePassword: "~iconShowHidePassword",
   },
   messageErrorFields: {
-    errorPIN: "~",
-    errorOTPcode: "~",
+    errorPIN: ~"textErrorPin",
+    errorOTPcode: ~"textErrorOtp",
+    errorPassword: ~"textErrorPassword",
+    blockPassword: ~"textBlockPassword",
+  },
+  texts: {
+    email: ~"textEmail",
   },
   submitPassword(){
     I.waitForElement(this.buttons.next,5); 
@@ -29,11 +36,11 @@ module.exports = {
   },
   inputPIN(pin){
     I.waitForElement(this.fields.pin,5);
-    I.setText(this.fields.newPIN,pin)
+    I.setText(this.fields.newPIN,pin);
   },
   inputConfirmationPIN(confirmationPIN){
     I.waitForText("Konfirmasi PIN Baru",10);
-    I.setText(this.fields.newPIN,confirmationPIN);  
+    I.setText(this.fields.confirmPIN,confirmationPIN);
   },
   inputOTP(OTPcode){
     I.waitForElement(this.fields.otpcode,5);
@@ -50,6 +57,22 @@ module.exports = {
     I.waitForElement(this.messageErrorFields.errorOTPcode,5);
     return await I.grabTextFrom(this.messageErrorFields.errorOTPcode);
   },
-
+  async getMessageErrorPassword(){
+    I.waitForElement(this.messageErrorFields.errorPassword,5);
+    return await I.grabTextFrom(this.messageErrorField.errorPasswords);
+  },
+  async getmessageBlockPassword(){
+    I.waitForElement(this.messageErrorFields.blockPassword,5);
+    return await I.grabTextFrom(this.messageErrorFields.blockPassword);
+  },
+  submitTryAgainPassword(){
+    I.click(this.buttons.tryagainpassword);
+  },
+  reactiveAccount(){
+    I.click(this.buttons.reactiveAccount);
+  },
+  async getconfirmemail(){
+    return await I.grabTextFrom(this.texts.email);  
+ },
 }
  
