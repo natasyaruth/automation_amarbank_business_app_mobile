0e72deb20c7334519184513cce8d78cdcc911d3c
Mike Reborn <michael.william@amarbank.co.id>
2024-01-26 12:33:59 +0700

    update for consent PDP automation

--- pages/registration.js
+++ pages/registration.js
@@ -40,13 +40,13 @@ module.exports = {
   },
   checkButton:{
     firstPdp: "~cbShareValidInfo",
-    secondPdp: "~cbShareToOthers",
+    secondPdp: "~cBShareToOthers",
     thirdPdp: "~cbReceiveInfoProduct",
   },
   text:{
-    firstPdpText: "Membagikan data dan/atau informasi pribadi secara benar, lengkap, asli, sah dan sesuai peraturan perundang-undangan yang berlaku kepada Bank sebagai syarat penggunaan produk atau layanan Bank.",
-    secondPdpText: "Membagikan penggunaan data dan/atau informasi pribadi oleh/kepada pihak ketiga untuk tujuan penggunaan produk atau layanan Bank.",
-    thirdPdpText: "Menerima penawaran produk dan/atau layanan melalui sarana komunikasi pribadi nasabah sebagai syarat penggunaan produk atau layanan Bank.",
+    firstPdpText: { xpath: '//android.widget.TextView[contains(@text, //android.widget.TextView[@text="Membagikan data dan/atau informasi pribadi secara benar, lengkap, asli, sah dan sesuai peraturan perundang-undangan yang berlaku kepada  Bank sebagai syarat penggunaan produk atau layanan Bank. *"])]' },
+    secondPdpText: { xpath: '//android.widget.TextView[contains(@text, //android.widget.TextView[@text="Membagikan penggunaan data dan/atau informasi pribadi oleh/kepada pihak ketiga untuk tujuan penggunaan produk atau layanan Bank. *"])]' },
+    thirdPdpText: { xpath: '//android.widget.TextView[contains(@text, //android.widget.TextView[@text="Menerima penawaran produk dan/atau layanan melalui sarana komunikasi pribadi nasabah sebagai syarat penggunaan produk atau layanan Bank."])]' },
   },
 
   fillInAccountInformation(accountInformation) {
@@ -105,6 +105,31 @@ module.exports = {
     return await I.grabTextFrom(this.label[labelName]);
   },
 
+  async checkTnC(){
+
+    I.waitForElement(this.buttons.createAccountPDP,5);
+    let isDisabled = await I.grabAttributeFrom(this.buttons.createAccountPDP, "clickable");
+
+    if(isDisabled === "false"){
+      console.log("Tombol dalam keadaan disable.");
+      //return true;
+    }
+
+    else{
+      console.log("Tombol dalam keadaan enable.");
+      //return true;
+    }
+  },
+
+  // async verifyButtonDisabled(){
+  //   let isDisabled = await I.grabAttributeFrom((this.buttons.createAccountPDP), 'disabled');
+  // if (isDisabled === 'true') {
+  // //console.log('The element is disabled');
+  // } else {
+  //   //console.log('The element is not disabled');
+  // }
+  // },
+
   clickIconEyePassword(){
     I.click(this.icons.eyePassword);
   },
@@ -114,7 +139,9 @@ module.exports = {
   },
 
   clickCreateAccountButton() {
-    I.swipeUp(this.fields.confirmPassword, 500, 1000);
+    //I.swipeUp(this.fields.confirmPassword, 500, 1000);
+    I.scrollIntoView(this.buttons.createAccount);
+    //I.waitForInvisible(this.buttons.backRegist, 3);
     I.click(this.buttons.createAccount);
   },
 
