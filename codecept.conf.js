0a1bac1da400a543ce9ed86d6bec77cc31ccc949
Nizar Nugraha <nizar.amarbank.co.id>
2024-02-19 15:35:01 +0700

    rename config

--- codecept.conf.js
+++ codecept.conf.js
@@ -31,8 +31,8 @@ exports.config = {
         automationName: "UiAutomator2",
         newCommandTimeout: 300,
         // deviceName: "emulator-5554",
-        appPackage: "id.co.amarbank.smb.dev.staging", //staging
-        // appPackage: "id.co.amarbank.smb.dev.dev", //Dev
+        appPackage: "id.co.amarbank.smb.staging", //staging
+        // appPackage: "id.co.amarbank.smb.dev", //Dev
         appActivity: "id.co.amarbank.smb.ui.MainActivity",
         autoGrantPermissions: true
       }
