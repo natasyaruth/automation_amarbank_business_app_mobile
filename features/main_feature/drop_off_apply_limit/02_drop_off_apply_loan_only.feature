ed63433beea76d1642518d2ec6c8352347ef03d4
nurulsepta <nurul.septriani@amarbank.co.id>
2024-10-10 13:45:19 +0700

    test case for sprint 1 Q4

--- features/main_feature/drop_off_apply_limit/02_drop_off_apply_loan_only.feature
+++ features/main_feature/drop_off_apply_limit/02_drop_off_apply_loan_only.feature
@@ -20,7 +20,7 @@ Feature: Apply Loan Only with new drop off management
 
 
 
-Scenario: User apply loan only drop off before submit data anchor
+Scenario: User apply loan only drop off before submit detail kredit
     Given User click button loan dashboard
     When user click button Ajukan Limit Baru 
     And User select loan type "AP"
@@ -30,18 +30,10 @@ Scenario: User apply loan only drop off before submit data anchor
     And user click button Save
     And user input tenor "30"
     And user click button Lanjut Isi Data Supplier
-    #section select Anchor
-    When user on buyer cooperating page
-    And user select another supplier
-    And user fill a field "anchorName" with "AP Direct Tes"
-    And user select industry type
-    And user select the date cooperating
-    And user input business address
-    #section supplier representatives has contact
-    And user input supplier representatives name
-    And user input contact name
-    And user input email address supplier
-    And user click back button
+    And click back to Detail kredit page
+    And back to select loan type page
+    And back to Main dashboard
+
 
 Scenario: User validate status Pengajuan Limit on apply loan only
     Given user on main dashboard
@@ -51,12 +43,14 @@ Scenario: User validate status Pengajuan Limit on apply loan only
     And user should see text card "Proses" on field "textCardProcess"   
     And user click button Aktivitas Pinjaman
     And user on Aktvitas Pinjaman page
-    And user see text card Pengajuan Lmit
+    And user see text card Pengajuan Limit
+
 
 Scenario: User continue journey apply loan only from status Pengajuan Limit
     Given user on Aktivitas Pinjaman
     When user click card with status Pengajuan Limit 
-    And user on Ajukan Pinjaman Bisnis Page
+    And user on Detail Kredit Page
+    And user click button Lanjut isi data supplier
     And user on buyer cooperating page
     And user select another buyer
     And user fill a field "anchorName" with "AP Direct Tes"
--- pages/loanApplication/limitActive.js
+++ pages/loanApplication/limitActive.js
@@ -10,13 +10,30 @@ module.exports = {
     headerLimitActive: { xpath: '//android.widget.TextView[contains(@text, "Selamat, Limit Pinjaman Telah Aktif")]' },
     textViewLimitTersedia: { xpath: '//android.widget.TextView[contains(@text, "Limit Tersedia")]' },
     textViewLimitSupplier: { xpath: '//android.widget.TextView[contains(@text, "Supplier")]' },
+    textViewLimitBuyer: { xpath: '//android.widget.TextView[contains(@text, "Buyer")]' },
+    textViewLimitBouwheer: { xpath: '//android.widget.TextView[contains(@text, "Bouwheer")]' },
     textViewLimitNoPinjaman: { xpath: '//android.widget.TextView[contains(@text, "No. Pinjaman")]' },
-    textViewLimitInformasiTambahan: { xpath: '//android.widget.TextView[contains(@text, "Informasi Tambahan")]' }
+    textViewLimitInformasiTambahan: { xpath: '//android.widget.TextView[contains(@text, "Informasi Tambahan")]' },
+    textviewheaderRejectPage: { xpath: '//android.widget.TextView[contains(@text, "Mohon Maaf, Permohonan limit Anda belum disetujui")]' },
+
+
   },
   buttons: {
     btnBack: "~buttonBack",
     btnCopy: "~buttonCopy",
     btnBackToDashboard: "~buttonBackToDashboard"
+  },
+
+  buttons: {
+    onestar: "~buttonOnestar",
+    twostar: "~buttonTwostar",
+    threestar: "~buttonThreestar",
+    fourstar: "~buttonFourstar",
+    fivestar: "~buttonFivestar",
+    sendLending: "~sendfeedbacklending",
+  },
+  fields: {
+    feedbacklending: "~textfieldsfeedback",
   },
 
   //
@@ -32,4 +49,51 @@ module.exports = {
     I.wait(2);
     I.click(this.buttons.btnBackToDashboard);
   },
+
+
+  clickRatingLending(index){
+
+    switch(index){
+
+        case "1":
+          I.waitForElement(this.buttons.onestar, 10);
+          I.click(this.buttons.onestar);
+          break;
+        case "2":
+            I.waitForElement(this.buttons.twostar,10);
+            I.click(this.buttons.twostar);
+            break;
+        case "3":
+            I.waitForElement(this.buttons.threestar,10);
+            I.click(this.buttons.threestar);
+            break;
+        case "4":
+            I.waitForElement(this.buttons.fourstar, 10);
+            I.click(this.buttons.fourstar);
+            break;
+        case "5":
+            I.waitForElement(this.buttons.fivestar, 10);
+            I.click(this.buttons.fivestar);
+            break;
+            default:
+                throw new Error("Please choose rating with range 1-5 stars");         
+        }
+},
+
+InputFeedback(text) {
+    I.waitForElement(this.fields.feedbacklending,30);
+    I.click(this.fields.feedbacklending);
+    I.type(text, 100);
+    I.hideDeviceKeyboard();
+},
+
+sendFeedbacklending(){
+    I.waitForElement(this.buttons.sendLending, 10);
+    I.click(this.buttons.sendLending);
+},
+    
+
 }
+
+
+
